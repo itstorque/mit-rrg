a4ca9dbc99242db103fc22afa9e99a96812b33eb
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-08-16 01:23:51 +0400

    Added htaccess sync and collection support for fetch_resources

--- _dev/fetch_resources.sh
+++ _dev/fetch_resources.sh
@@ -1,4 +1,7 @@
 dir=".";
+final="web_scripts"
+
+path=${PWD##*/};
 
 if [ $path == "_dev" ]; then
 	dir="..";
@@ -8,6 +11,7 @@ url=https://esiresponse.github.io;
 
 if [ $1 == "-d" ]; then
 	url=http://localhost:4000/mit-rrg;
+	final="_site";
 fi
 
 data=$(curl -L "$(echo $url)/scripts_sync/resource_map");
@@ -20,10 +24,18 @@ IFS=',' read -r -a files <<< "$data";
 
 for file in "${files[@]}"
 do
+	if [ "${file: -1}" == "~" ]; then
+		target="${file: -1}.html";
+		file="${file: -1}/";
+	else
+		target=$file;
+	fi
     echo "$url$file";
-	curl -L "$url$file" -o "$dir/_fetch_cache" --create-dirs
+	curl -L "$url$file" -o "$dir/_fetch_cache$target" --create-dirs
 done
 
-rm -rf "$dir/_site";
+rm -rf "$dir/$final";
 
-mv "$dir/_fetch_cache" "$dir/_site";
+mv "$dir/_fetch_cache" "$dir/$final";
+
+mv "$dir/$final/scripts_sync/htaccess.html" "$dir/$final/.htaccess";
