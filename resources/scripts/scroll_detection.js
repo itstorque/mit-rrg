e539fc43f2d42b6d3a6d2bc4c51cd459058e63e7
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-08-17 09:05:51 +0400

    Addded tons of responsive elements and finished responsive menu

--- resources/scripts/scroll_detection.js
+++ resources/scripts/scroll_detection.js
@@ -3,19 +3,23 @@ window.addEventListener('load', (event) => {
 	is_home_page = document.title=="Home";
 	offset = 40;
 
-	if (is_home_page) {
+	resize_func = (event) => {
+
+		document.body.classList.add("scrolled");
 
 		offset = window.innerHeight*0.7+40;
+
+		if (window.innerWidth < 800) {
+			offset = window.innerHeight*0.1+40;
+		}
+
+		console.log("resize", window.innerWidth < 800, offset)
+
+		scroll_func();
 
 	}
 
-	window.addEventListener('resize',(event) => {
-
-		offset = window.innerHeight*0.7+40;
-
-	}, { passive: true });
-
-	window.addEventListener('scroll',(event) => {
+	scroll_func = (event) => {
 
 		if (window.scrollY > offset && !document.body.classList.contains("scrolled")) {
 			document.body.classList.add("scrolled");
@@ -23,6 +27,16 @@ window.addEventListener('load', (event) => {
 			document.body.classList.remove("scrolled");
 		}
 
-	}, { passive: true });
+		console.log("scroll", window.scrollY <= offset)
+
+	}
+
+	resize_func();
+
+	window.addEventListener('resize', resize_func, { passive: true });
+
+	window.addEventListener('scroll', scroll_func, { passive: true });
+
+	scroll_func()
 
 });
